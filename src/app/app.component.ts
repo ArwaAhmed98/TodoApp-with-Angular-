efea103266b56492622b39cd1dd3fff83dd54c73
ArwaAhmed98 <arwaahmedmahmoud99.com>
2020-08-20 16:26:12 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ToDoList';
+  title = ' My ToDoList';
+  todos = [
+    {label:'Bring the Milk',done:false,Priority : 1 },
+    {label:'Pay the bill',done:true,Priority : 3 },
+    {label:'Clean the house',done:false,Priority : 2 },
+    {label:'Fix the bulb',done:true,Priority : 4}
+  ];
+
+  addTodo(newTodoLabel , newtodoP){
+    var newTodo = {
+      label: newTodoLabel,
+      Priority: newtodoP,
+      done:false 
+  };
+  this.todos.push(newTodo);
+}
+
+
+deleteTodo(todo){
+  this.todos = this.todos.filter(t => (t.label !== todo.label));
+}
+
 }
